b55dfdf8974dc1e59468812ec6af85b55905e810
Renato033 <rg202436@gmail.com>
2026-04-15 21:17:50 -0300

    Add files via upload

---
+++ aula014/script.js
@@ -0,0 +1,12 @@
+/* let nome = "Rafael"
+alert(nome.toUpperCase())
+alert(nome.toUpperCase()) */
+
+/* let dinheiro = 5000
+alert(dinheiro.toLocaleString(`pt-BR`, {
+  style: `currency`,
+  currency: `USD`,
+
+})) */
+let fruta = "Uva"
+alert(fruta.length)
